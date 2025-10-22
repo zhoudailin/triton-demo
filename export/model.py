7d985742cdc01d77ac4dc592ef8223acfdb04da1
mrzhou <zhoudailincser@gmail.com>
2025-10-22 20:32:50 +0800

    init

---
+++ export/model.py
@@ -0,0 +1,13 @@
+from funasr import AutoModel
+from funasr_onnx import Paraformer
+
+# model = AutoModel(
+#     model="paraformer-zh-streaming",
+#     disable_update=True
+# )
+# model.export(output_dir="model")
+model = Paraformer(
+    model_dir='iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online'
+)
+res = model('/home/mrzhou/Documents/codes/private/triton-demo/data/asr_example.wav')
+print(res)
