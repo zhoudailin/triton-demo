53f6cd7c12d259fd2720910b0332d7818bbb738b
zhoudailin <dailinzhoucser@gmail.com>
2025-11-25 21:22:42 +0800

    提交代码

--- triton/feature_extractor/1/feat.py
+++ triton/feature_extractor/1/feat.py
@@ -1,31 +1,32 @@
-import torch
+import numpy as np
 
 
 class Feat(object):
     def __init__(self, corrid, offset_ms, sample_rate, frame_stride):
         self.corrid = corrid
         self.sample_rate = sample_rate
-        self.audios: torch.Tensor = torch.tensor([])
+        self.audios: np.ndarray = np.array([])
         self.offset = int(offset_ms / 1000 * sample_rate)
         self.frames = None
         self.frame_stride = int(frame_stride)
         self.lfr_m = 7
 
-    def add_audio(self, audio: torch.Tensor):
-        self.audios = torch.cat((self.audios, audio))
+    def add_audio(self, audio: np.ndarray):
+        print('add_audio: ', self.audios, audio)
+        self.audios = np.concatenate([self.audios, audio])
 
     def get_seg_wav(self):
         seg = self.audios[:]
         self.audios = self.audios[-self.offset:]
         return seg
 
-    def add_frames(self, frames: torch.Tensor):
+    def add_frames(self, frames: np.ndarray):
         if self.frames is None:
-            self.frames = torch.cat(
+            self.frames = np.concatenate(
                 (frames[0, :].repeat((self.lfr_m - 1) // 2, 1), frames)
             )
         else:
-            self.frames = torch.cat([self.frames, frames])
+            self.frames = np.concatenate([self.frames, frames])
 
     def get_frames(self, num_frames: int):
         seg = self.frames[0:num_frames]
--- triton/feature_extractor/1/model.py
+++ triton/feature_extractor/1/model.py
@@ -18,7 +18,7 @@ sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8", line_bufferin
 
 class TritonPythonModel:
     model_config: Dict
-    chunk_size: float
+    chunk_size: int
     parameters: Dict
     chunk_size_s: float
     config_path: str
@@ -31,7 +31,6 @@ class TritonPythonModel:
     sample_rate: float
 
     def initialize(self, args):
-        print('args: ', args)
         self.model_config = json.loads(args["model_config"])
         output0_config = pb_utils.get_output_config_by_name(self.model_config, "speech")
 
@@ -42,18 +41,21 @@ class TritonPythonModel:
             self.parameters[key] = value
         self.chunk_size_s = float(self.parameters["chunk_size_s"])
         self.config_path = self.parameters["config_path"]
+
         with open(self.config_path, "r", encoding="utf-8") as f:
             self.config = yaml.load(f, Loader=yaml.FullLoader)
+
         self.feature_size = output0_config["dims"][-1]
         self.decoding_window = output0_config["dims"][-2]
 
+        frontend_conf = self.config["frontend_conf"]
         opts = knf.FbankOptions()
-        opts.frame_opts.dither = 0.0  # 随机噪声
-        opts.frame_opts.window_type = self.config["frontend_conf"]["window"]  # 窗函数
-        opts.mel_opts.num_bins = int(self.config["frontend_conf"]["n_mels"])  # 梅尔频带数
-        opts.frame_opts.frame_shift_ms = float(self.config["frontend_conf"]["frame_shift"])  # 帧位移
-        opts.frame_opts.frame_length_ms = float(self.config["frontend_conf"]["frame_length"])  # 帧长度
-        opts.frame_opts.samp_freq = int(self.config["frontend_conf"]["fs"])  # 帧率
+        opts.frame_opts.dither = 0.0
+        opts.frame_opts.window_type = frontend_conf["window"]  # 窗函数
+        opts.mel_opts.num_bins = int(frontend_conf["n_mels"])  # 梅尔频带数
+        opts.frame_opts.frame_shift_ms = float(frontend_conf["frame_shift"])  # 帧位移
+        opts.frame_opts.frame_length_ms = float(frontend_conf["frame_length"])  # 帧长度
+        opts.frame_opts.samp_freq = int(frontend_conf["fs"])  # 帧率
         self.opts = opts
         self.fbank = knf.OnlineFbank(self.opts)
 
@@ -65,7 +67,7 @@ class TritonPythonModel:
 
         print(f'-------------\n{self.chunk_size_s} {sample_rate}')
         print(f'-------------\n{type(self.chunk_size_s)} {type(sample_rate)}')
-        self.chunk_size = self.chunk_size_s * sample_rate
+        self.chunk_size = int(self.chunk_size_s * sample_rate)
         self.frame_stride = (self.chunk_size_s * 1000) // frame_shift_ms
         self.offset_ms = self.get_offset(frame_length_ms, frame_shift_ms)
         self.sample_rate = sample_rate
@@ -79,19 +81,17 @@ class TritonPythonModel:
 
     def execute(self, requests):
         responses = []
-        total_waves = []
+        total_waves = np.array([])
         batch_seqid = []
         end_seqid = {}
         for request in requests:
             input0 = pb_utils.get_input_tensor_by_name(request, "wav")
             print('----------input0:', input0, type(input0))
-            wav = input0.as_numpy()
+            wav = input0.as_numpy()[0]
             print(wav, type(wav), wav.shape)
             wav_len = len(wav)
             if wav_len < self.chunk_size:
-                temp = torch.zeros(
-                    self.chunk_size, dtype=torch.float32, device=self.device
-                )
+                temp = np.zeros(self.chunk_size, dtype=np.float32)
                 temp[0:wav_len] = wav[:]
                 wav = temp
 
@@ -117,8 +117,9 @@ class TritonPythonModel:
             if end:
                 end_seqid[corrid] = 1
             wav = self.seq_feat[corrid].get_seg_wav() * 32768
-            total_waves.append(wav.cpu() if wav.is_cuda else wav)
-        self.fbank.accept_waveform(16000, total_waves)
+            np.concatenate([total_waves, wav])
+        self.fbank.accept_waveform(16000, total_waves.tolist())
+
         for corrid, frames in zip(batch_seqid, features):
             self.seq_feat[corrid].add_frames(frames)
             speech = self.seq_feat[corrid].get_frames(self.decoding_window)
